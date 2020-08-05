63215f9e51c1dd997907e73677d653000ce76db6
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2020-08-05 14:03:05 +0200

    Create action and workflow for releases

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 from alpine:latest
 
-run apk add python3
-run pip3 install chevron xmltodict
+run apk update
+run apk add python3 py3-pip
+run /usr/bin/pip3 install chevron xmltodict
 copy . /xml2md
 workdir /xml2md
 cmd /xml2md/process_test_files.sh
--- process_test_files.sh
+++ process_test_files.sh
@@ -1,10 +1,13 @@
 
+OUTPUT_DIR="/github/workspace/output/"
+mkdir -p ${OUTPUT_DIR}
+
 find xml2md-test -type f -name '*.xml' -exec sh -c '
-  for file do
-    base_name=$(basename "$file")
-    echo $base_name
-    output_file_name="output/${base_name}.md"
-    echo $output_file_name
-    python3 xml2md.py "$file" > "$output_file_name"
-  done
-' exec-sh {} +
+  file="$0"
+  OUTPUT_DIR="$1"
+  base_name=$(basename "$file")
+  echo $base_name
+  output_file_name="${OUTPUT_DIR}/${base_name}.md"
+  echo $output_file_name
+  python3 xml2md.py "$file" > "$output_file_name"
+' {} ${OUTPUT_DIR} ';'
