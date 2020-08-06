7ada3262a4a1db4d7205cbfe0c02b5d79563a2df
Achim Völker <achim.voelker@rwth-aachen.de>
2020-08-06 10:25:23 +0200

    add addLibrarymethod, fix intendation

--- xml2md.py
+++ xml2md.py
@@ -13,12 +13,20 @@ def printMarkdown(dataObj, filename):
         args = { "template": template,
                  "data":     dataObj }
         print(chevron.render(**args))
- 
+
+def addLibraryObjects(usecase, xmlobj):
+    usecase['AreaLibrary'] = xmlobj['UC:UseCaseRepository']['AreaLibrary']
+    usecase['ActorLibrary'] = xmlobj['UC:UseCaseRepository']['ActorLibrary']
+    usecase['BusinessObjectLibrary'] = xmlobj['UC:UseCaseRepository']['BusinessObjectLibrary']
+    usecase['RequirementLibrary'] = xmlobj['UC:UseCaseRepository']['RequirementLibrary']
+    usecase['CommonTermLibrary'] = xmlobj['UC:UseCaseRepository']['CommonTermLibrary']
+    return usecase
+
 def main():
     for filename in sys.argv[1:]:
         with open(filename) as xmlfile:
             usecase = None
-            xmlobj=xmltodict.parse(xmlfile.read())
+            xmlobj = xmltodict.parse(xmlfile.read())
 
             if 'UC:UseCaseRepository' in xmlobj:
                 otherUseCases = []
@@ -27,26 +35,20 @@ def main():
                     if type(xmlobj['UC:UseCaseRepository']['UseCaseLibrary']['UseCase']) is list:
                         for usecase in xmlobj['UC:UseCaseRepository']['UseCaseLibrary']['UseCase']:
                             if 'scope' in usecase:
-                                usecase['AreaLibrary'] = xmlobj['UC:UseCaseRepository']['AreaLibrary']
-                                usecase['ActorLibrary'] = xmlobj['UC:UseCaseRepository']['ActorLibrary']
-                                usecase['BusinessObjectLibrary'] = xmlobj['UC:UseCaseRepository']['BusinessObjectLibrary']
-                                usecase['RequirementLibrary'] = xmlobj['UC:UseCaseRepository']['RequirementLibrary']
-                                usecase['CommonTermLibrary'] = xmlobj['UC:UseCaseRepository']['CommonTermLibrary']
-                                theUseCase = usecase
+                                theUseCase = addLibraryObjects(usecase, xmlobj)
                             else:
                                 otherUseCases.append(usecase)
                     elif isinstance(xmlobj['UC:UseCaseRepository']['UseCaseLibrary']['UseCase'], Mapping):
                         if 'scope' in xmlobj['UC:UseCaseRepository']['UseCaseLibrary']['UseCase']:
                             usecase = xmlobj['UC:UseCaseRepository']['UseCaseLibrary']['UseCase']
-                            usecase['AreaLibrary'] = xmlobj['UC:UseCaseRepository']['AreaLibrary']
-                            theUseCase = usecase
+                            theUseCase = addLibraryObjects(usecase, xmlobj)
                 if theUseCase:
                     theUseCase['otherUseCases'] = otherUseCases
                     printMarkdown(usecase, "UseCaseRepository.mustache")
-
             else:
+                print ("UseCase version")
+                print(xmlobj)
                 printMarkdown(xmlobj['UseCase'], "UseCase.mustache")
-
             xmlfile.close()
 
 if __name__ == "__main__":
