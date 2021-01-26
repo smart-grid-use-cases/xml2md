5fdd14406fc96a0b349d76087cc773bb6e2e2aff
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2021-01-26 12:42:33 +0100

    Use same copy function for both input directories
    Signed-off-by: Richard Marston <rmarston@eonerc.rwth-aachen.de>

--- process_test_files.sh
+++ process_test_files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -o errexit
+
 if [ -z "$1" ]
 then
   OUTPUT_DIR="/github/workspace/Bridge"
@@ -9,52 +11,43 @@ fi
 
 mkdir -p ${OUTPUT_DIR}
 
+RUN_XML2MD=$(cat << EOM
+  set -o errexit
+  FILE=\$0
+  OUTPUT_DIR="\$1"
+  PREFIX="\$2"
+  BASENAME_WITH_SUFFIX=\$(basename "\${FILE}")
+  BASENAME=\${BASENAME_WITH_SUFFIX%.xml}
+  DIRNAMEPREFIX=\$(dirname "\${FILE}")
+  DIRNAME=\${DIRNAMEPREFIX#\$PREFIX}/\${BASENAME}
+  mkdir -p "\${OUTPUT_DIR}/\${DIRNAME}/"
+  OUTPUT_FILE_NAME="\${OUTPUT_DIR}/\${DIRNAME}/index.md"
+  echo "Creating markdown file: \$OUTPUT_FILE_NAME"
+  python3 xml2md.py "\$FILE" > \$OUTPUT_FILE_NAME
+  IMAGES=\$(find "\${DIRNAMEPREFIX}" -iname *.png)
+  for IMAGE in \$IMAGES
+  do
+    echo Copying image file "\$IMAGE" into "\${OUTPUT_DIR}/\${DIRNAME}"
+    cp "\$IMAGE" "\${OUTPUT_DIR}/\${DIRNAME}"
+  done
+EOM
+)
+
 # process all *.xml files from grupoetra and create index.md
-find grupoetra/* -type f -name '*.xml' -exec sh -c '
-  set -x
-  FILE="$0"
-  OUTPUT_DIR="$1"
-  DIRNAMEPREFIX=$(dirname "${FILE}")
-  DIRNAME=${DIRNAMEPREFIX#"grupoetra/"}
-  mkdir -p ${OUTPUT_DIR}/${DIRNAME}
-  OUTPUT_FILE_NAME="${OUTPUT_DIR}/${DIRNAME}/index.md"
-  echo "Creating markdown file: $OUTPUT_FILE_NAME"
-  python3 xml2md.py "$FILE" > "$OUTPUT_FILE_NAME"
-' {} ${OUTPUT_DIR} ';'
+find grupoetra/* -type f -name '*.xml' -exec sh -c "$RUN_XML2MD" {} ${OUTPUT_DIR} "grupoetra/" ';'
 
 # process all *.xml files from xml2md-input and create index.md
-find xml2md-input/* -type f -name '*.xml' -exec sh -c '
-  file="$0"
-  OUTPUT_DIR="$1"
-  dirnameprefix=$(dirname "${file}")
-  DIRNAME=${dirnameprefix#"xml2md-input/"}
-  mkdir -p ${OUTPUT_DIR}/${DIRNAME}
-  output_file_name="${OUTPUT_DIR}/${DIRNAME}/index.md"
-  echo "Creating markdown file: $output_file_name"
-  python3 xml2md.py "$file" > "$output_file_name"
-' {} ${OUTPUT_DIR} ';'
-
-# cp all the *.png files into the output directory
-find xml2md-input/* -type f -name '*.png' -exec sh -c '
-  file="$0"
-  OUTPUT_DIR="$1"
-  basename=$(basename $file)
-  dirnameprefix=$(dirname "${file}")
-  DIRNAME=${dirnameprefix#"xml2md-input/"}
-  output_file_name="${OUTPUT_DIR}/${DIRNAME}/${basename}"
-  echo "Creating diagram file: $output_file_name"
-  cp $file $output_file_name
-' {} ${OUTPUT_DIR} ';'
+find xml2md-input/* -type f -name '*.xml' -exec sh -c "$RUN_XML2MD" {} ${OUTPUT_DIR} "xml2md-input/" ';'
 
 find "${OUTPUT_DIR}" -type d -exec sh -c '
   DIRPATH=$0
-  DIRNAME=$(basename $DIRPATH)
+  DIRNAME=$(basename "$DIRPATH")
   if [ ! -f "${DIRPATH}/index.md" ]
   then
     if [ ! -f "${DIRPATH}/_index.md" ]
     then
-      echo Creating title link for directory: $DIRPATH
-      cat > ${DIRPATH}/_index.md <<EOF
+      echo Creating title link for directory: $DIRPATH with title: $DIRNAME
+      cat > "${DIRPATH}/_index.md" <<EOF
 ---
 title: "$DIRNAME"
 linkTitle: "$DIRNAME"
