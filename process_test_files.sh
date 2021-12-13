a2f42f95145a355eb709ad292566e9291f0bc452
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2021-12-13 12:13:32 +0100

    Copy images with spaces in the filename
    Signed-off-by: Richard Marston <rmarston@eonerc.rwth-aachen.de>

--- process_test_files.sh
+++ process_test_files.sh
@@ -11,35 +11,11 @@ fi
 
 mkdir -p ${OUTPUT_DIR}
 
-RUN_XML2MD=$(cat << EOM
-  FILE=\$0
-  OUTPUT_DIR="\$1"
-  PREFIX="\$2"
-  DIRNAMEPREFIX=\$(dirname "\${FILE}")
-  IS_TLD=\$(echo \${DIRNAMEPREFIX} | grep "/")
-  if [ \$? -eq 1 ]; then
-    DIRNAME=/
-  else
-    DIRNAME=/\${DIRNAMEPREFIX#\$PREFIX}
-  fi
-  mkdir -p "\${OUTPUT_DIR}/\${DIRNAME}/"
-  OUTPUT_FILE_NAME="\${OUTPUT_DIR}\${DIRNAME}/index.md"
-  echo "Creating markdown file: \$OUTPUT_FILE_NAME"
-  python3 xml2md.py "\$FILE" > "\${OUTPUT_FILE_NAME}"
-  IMAGES=\$(find "\${DIRNAMEPREFIX}" -iname *.png)
-  for IMAGE in \$IMAGES
-  do
-    echo Copying image file "\$IMAGE" into "\${OUTPUT_DIR}/\${DIRNAME}"
-    cp "\$IMAGE" "\${OUTPUT_DIR}/\${DIRNAME}"
-  done
-EOM
-)
-
 # process all *.xml files from grupoetra and create index.md
-find grupoetra/* -type f -name '*.xml' -exec sh -c "$RUN_XML2MD" {} ${OUTPUT_DIR} "grupoetra/" ';'
+find grupoetra/* -type f -name '*.xml' -exec ./run_xml2md.sh {} ${OUTPUT_DIR} "grupoetra/" ';'
 
 # process all *.xml files from xml2md-input and create index.md
-find xml2md-input/* -type f -name '*.xml' -exec sh -c "$RUN_XML2MD" {} ${OUTPUT_DIR} "xml2md-input/" ';'
+find xml2md-input/* -type f -name '*.xml' -exec ./run_xml2md.sh {} ${OUTPUT_DIR} "xml2md-input/" ';'
 
 find "${OUTPUT_DIR}" -type d -exec sh -c '
   DIRPATH=$0
